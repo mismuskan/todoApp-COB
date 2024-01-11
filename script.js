2f09e5816f905515bdeabe6761602c76003b0897
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-11 19:31:55 +0530

    todo

---
+++ script.js
@@ -0,0 +1,43 @@
+let btn=document.querySelector("#add");
+let input=document.querySelector("#taskInput");
+let list=document.querySelector("#taskList");
+let count=0;
+    let maxCount=10;
+btn.addEventListener("click",()=>{
+    let taskValue=input.value;
+    
+    if (taskValue.trim() !== "" && count<=maxCount) {
+        let listItem = document.createElement("li");
+    
+        listItem.textContent = taskValue;
+
+        let removeButton = document.createElement("button");
+        removeButton.classList.add("remove");
+        removeButton.innerHTML = "&times;"; 
+        removeButton.addEventListener("click", () => {
+          list.removeChild(listItem);
+          count--;
+          btn.disabled = false;
+          input.disabled=false;
+        });
+  
+        listItem.appendChild(removeButton);
+
+        list.appendChild(listItem);
+        count++;
+        input.value = "";
+      } 
+      if (count === maxCount) {
+        btn.disabled = true;
+        input.disabled=true;
+        input.placeholder="Ohh! Limit Exceeded..";
+        input.classList.add("input");
+        // input.style.color="white";
+      }
+      else {
+        removeButton.disabled=true;
+        
+        console.log("Please enter a valid task!");
+      }
+
+});
